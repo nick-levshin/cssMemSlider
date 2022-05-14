29320bea4504832fc3b11610c519577d117299ac
Nick Levshin <nikita.levshin2016@gmail.com>
2022-05-14 19:58:58 +0300

    feat: add json data (Sat, May 14, 2022 19:58:11 PM)

---
+++ src/js/index.js
@@ -0,0 +1,13 @@
+import { Slider } from './Slider.js';
+
+let s;
+fetch('./src/js/slides.json').then(async (response) => {
+  try {
+    const data = await response.json();
+    s = new Slider(data);
+    s.render();
+  } catch (e) {
+    console.log('Something went wrong!');
+    console.error(e);
+  }
+});
